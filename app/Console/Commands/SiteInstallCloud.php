a2bf063cbdd9a452c6a64a1c18ecd777ada33f94
OUDRHIRI Oussama <oudrhiriouss@gmail.com>
2021-03-03 00:00:16 +0100

    cloud storage configuration artisan command site:installCloud

---
+++ app/Console/Commands/SiteInstallCloud.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SiteInstallCloud extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'site:installCloud {--force : Do not ask for user confirmation}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install Cloud setup data for the Application';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if ($this->option('force')) {
+            $this->proceed();
+        } else {
+            if ($this->confirm('This will delete ALL your current data and install the default setup data. Are you sure?')) {
+                $this->proceed();
+            }
+        }
+    }
+
+
+    protected function proceed(){   
+
+        $this->info('Migrating the database tables');
+
+        try {
+            $this->call('migrate:fresh', [
+                '--seed' => true,
+                '--force' => true,
+            ]);
+        } catch (\Exception $e) {
+            $this->error('Your tables are NOT seeded correctly.');
+        }
+        $this->info('Seeding data in the database tables');
+
+        $this->call('db:seed', [
+            '--class' => 'VoyagerDatabaseSeeder',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'DataTypesTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'DataRowsTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'MenuItemsTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'RolesTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'PermissionsTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'PermissionRoleTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'UsersTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->call('db:seed', [
+            '--class' => 'SettingsTableSeederCustom',
+            '--force' => true,
+        ]);
+
+        $this->info('cloud storage configured and site installed successfully!!');
+    }
+}
